4aa7c791b3e72df365d26b36001c0544eaf5c81c
Tom Richards <tom@tomrichards.net>
2017-09-27 23:53:19 -0400

    Update docs and types for size

--- src/tsc/command/setup.cr
+++ src/tsc/command/setup.cr
@@ -1,5 +1,6 @@
 module Tsc
   module Command
+    # This module implements **Setup and System Commands**.
     module Setup
       SIZE           = "SIZE"
       GAP            = "GAP"
@@ -47,8 +48,22 @@ module Tsc
         @socket << EOL
       end
 
-      # This command defines the label width and length.
-      def size(width : Int32, length : Int32, unit : Tsc::Unit = Tsc::Unit::Inch)
+      # Defines the label width and length.
+      #
+      # **Since:**
+      # - Dot units of measurement for this command has been supported since V6.27 EZ and later firmware.
+      #
+      # ```
+      # # 3.5in x 3.0in label
+      # printer.size(3.5, 3.0)
+      #
+      # # 100mm x 100mm label
+      # printer.size(100, 100, Tsc::Unit::Mm)
+      #
+      # # 120dot x 120dot label
+      # printer.size(120, 120, Tsc::Unit::Inch)
+      # ```
+      def size(width : Number, length : Number, unit : Tsc::Unit = Tsc::Unit::Inch)
         @socket << "#{SIZE} "
         case unit
         when Tsc::Unit::Inch
