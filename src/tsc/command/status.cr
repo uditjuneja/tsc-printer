67f4faff65380fc6949bf6a2314a90c56df0e037
Tom Richards <tom@tomrichards.net>
2017-09-27 23:20:43 -0400

    Sort methods, update docs

--- src/tsc/command/status.cr
+++ src/tsc/command/status.cr
@@ -2,30 +2,139 @@ require "../codemap"
 
 module Tsc
   module Command
+    # This module implements **Status Polling and Immediate Commands**.
     module Status
-      STATUS                 = "\e!?"
-      RESTART                = "\e!C"
-      DISABLE_IMMEDIATE      = "\e!D"
-      UNPAUSE                = "\e!O"
-      PAUSE                  = "\e!P"
-      RESTART_ONLY_AUTO      = "\e!Q"
-      RESET                  = "\e!R"
-      STATUS_DETAILED        = "\e!S"
-      FEED                   = "\e!F"
-      CANCEL                 = "\e!."
-      MILEAGE                = "\e~!@"
-      MEMORY                 = "\e~!A"
-      RTC                    = "\e~!C"
-      DUMP                   = "\e~!D"
-      IMMEDIATE              = "\e~!E"
-      FILES                  = "\e~!F"
-      CODE_PAGE_COUNTRY_CODE = "\e~!I"
-      MODEL                  = "\e~!T"
-      LINE_MODE_ENABLE       = "\eY"
-      LINE_MODE_DISABLE      = "\eZ"
+      # Immediate commands
 
-      # This command obtains the printer status at any time, even in the event
-      # of printer error.
+      CANCEL            = "\e!."
+      DISABLE_IMMEDIATE = "\e!D"
+      FEED              = "\e!F"
+      PAUSE             = "\e!P"
+      RESET             = "\e!R"
+      RESTART           = "\e!C"
+      RESTART_ONLY_AUTO = "\e!Q"
+      STATUS            = "\e!?"
+      STATUS_DETAILED   = "\e!S"
+      UNPAUSE           = "\e!O"
+
+      # Non-immediate commands
+
+      CODE_PAGE_COUNTRY_CODE = "\e~!I"
+      DUMP                   = "\e~!D"
+      ENABLE_IMMEDIATE       = "\e~!E"
+      FILES                  = "\e~!F"
+      LINE_MODE_DISABLE      = "\eZ"
+      LINE_MODE_ENABLE       = "\eY"
+      MEMORY                 = "\e~!A"
+      MILEAGE                = "\e~!@"
+      MODEL                  = "\e~!T"
+      RTC                    = "\e~!C"
+
+      # Cancels all printing files.
+      #
+      # This is an immediate command.
+      #
+      # **Since:**
+      # - This command has been supported since V7.00 EZ and later firmware.
+      #
+      # ```
+      # printer.cancel
+      # ```
+      def cancel
+        @socket << CANCEL
+      end
+
+      # Disables all immediate commands.
+      #
+      # This is an immediate command.
+      #
+      # **Since:**
+      # - This command has been supported since V6.61 EZ and later firmware.
+      #
+      # ```
+      # printer.disable_immediate
+      # ```
+      def disable_immediate
+        @socket << DISABLE_IMMEDIATE
+      end
+
+      # Feeds a label. This function is the same as pressing the FEED button.
+      #
+      # This is an immediate command.
+      #
+      # **Since:**
+      # - This command has been supported since V7.00 EZ and later firmware.
+      #
+      # ```
+      # printer.feed
+      # ```
+      def feed
+        @socket << FEED
+      end
+
+      # Pauses the printer.
+      #
+      # This is an immediate command.
+      #
+      # **Since:**
+      # - This command has been supported since V6.93 EZ and later firmware.
+      #
+      # ```
+      # printer.pause
+      # ```
+      def pause
+        @socket << PAUSE
+      end
+
+      # Resets the printer. Files downloaded in memory will be deleted. This command cannot be sent in dump mode.
+      #
+      # This is an immediate command.
+      #
+      # ```
+      # printer.reset
+      # ```
+      def reset
+        @socket << RESET
+      end
+
+      # Restarts the printer and does not run AUTO.BAS.
+      #
+      # This is an immediate command.
+      #
+      # **Note:**
+      # - When the printer receives this command, the printer will restart itself whether AUTO.BAS exists or not.
+      #
+      # **Since:**
+      # - This command has been supported since V5.23 EZ and later firmware.
+      #
+      # ```
+      # printer.restart
+      # ```
+      def restart
+        @socket << RESTART
+      end
+
+      # Restarts the printer only if AUTO.BAS exists.
+      #
+      # This is an immediate command.
+      #
+      # **Note:**
+      # - If there is no AUTO.BAS file loaded on the printer, the printer will not restart itself.
+      #
+      # **Since:**
+      # - This command has been supported since V6.72 EZ and later firmware.
+      #
+      # ```
+      # printer.restart_only_auto
+      # ```
+      def restart_only_auto
+        @socket << RESTART_AUTO_BAS
+      end
+
+      # Obtains the printer status at any time, even in the event of printer
+      # error.
+      #
+      # This is an immediate command.
       #
       # ```
       # printer.status # => :normal
@@ -37,50 +146,8 @@ module Tsc
         Codemap::DETAILS[resp]
       end
 
-      # This command restarts the printer and does not run AUTO.BAS.
-      #
-      # **Note:**
-      # - When the printer receives this command, the printer will restart itself whether AUTO.BAS exists or not.
-      #
-      # **Since:**
-      # - This command has been supported since V5.23 EZ and later firmware.
-      def restart
-        @socket << RESTART
-      end
-
-      # This command restarts the printer only if AUTO.BAS exists.
-      #
-      # **Note:**
-      # - If there is no AUTO.BAS inside the printer, the printer will not restart itself.
-      #
-      # **Since:**
-      # - This command has been supported since V6.72 EZ and later firmware.
-      def restart_only_auto
-        @socket << RESTART_AUTO_BAS
-      end
-
-      # This command resets the printer.
-      def reset
-        @socket << RESET
-      end
-
-      # This command is used to disable immediate commands.
-      def disable_immediate
-        @socket << DISABLE_IMMEDIATE
-      end
-
-      # This command is used to cancel the pause status of printer.
-      def unpause
-        @socket << UNPAUSE
-      end
-
-      # This command is used to pause the printer.
-      def pause
-        @socket << PAUSE
-      end
-
-      # This command obtains detailed status information of the printer, even
-      # in the event of printer error.
+      # Obtains detailed status information of the printer, even in the event of
+      # printer error.
       #
       # **Since:**
       # - This command has been supported since V6.29 EZ and later firmware.
@@ -96,17 +163,38 @@ module Tsc
         Codemap.convert_detailed_status_response(slice)
       end
 
-      # This command retrieves the mileage of the printer. Only the integer part
-      # of the mileage value is returned.
-      def mileage
+      # Retrieves the mileage of the printer.
+      #
+      # This is an immediate command.
+      #
+      # ```
+      # printer.mileage # => 89
+      # ```
+      def mileage : Int32
         @socket << MILEAGE
         resp = @socket.gets('\r', true)
-        return resp.to_i if resp
-
-        resp
+        resp.not_nil!.to_i
       end
 
-      # This command determines whether a Real Time Clock is installed in the printer.
+      # Cancels the pause status of printer.
+      #
+      # This is an immediate command.
+      #
+      # **Since:**
+      # - This command has been supported since V6.93 EZ and later firmware.
+      #
+      # ```
+      # printer.unpause
+      # ```
+      def unpause
+        @socket << UNPAUSE
+      end
+
+      # Determines whether a Real Time Clock is installed in the printer.
+      #
+      # ```
+      # printer.has_rtc? # => true
+      # ```
       def has_rtc? : Bool
         @socket << RTC
         resp = @socket.read_byte
@@ -115,18 +203,10 @@ module Tsc
         false
       end
 
-      # This command can cancel all printing files.
-      #
-      # **Since:**
-      # - This command has been supported since V7.00 EZ and later firmware.
-      def cancel
-        @socket << CANCEL
-      end
-
-      # This command retrieves the model name and number of the printer.
+      # Retrieves the model name and number of the printer.
       #
       # ```
-      # printer.model # => ""
+      # printer.model # => "TTP245C"
       # ```
       def model : String
         slice = Bytes.new(16)
